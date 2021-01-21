abfcb2c121d0787b31d5f01b5f775d994b4825a5
mariannaflores <68918978+mariannaflores@users.noreply.github.com>
2021-01-21 17:19:48 -0600

    Create Ejercicio 1.py

---
+++ Lab1/Ejercicio 1.py
@@ -0,0 +1,13 @@
+def triangulo_inv(n):
+
+    for i in range(n,0,-1):
+        for j in range(n-i):
+            print(' ', end=' ') 
+
+        for j in range(2*i-1):
+            print('*',end=' ') 
+        print() 
+
+triangulo_inv(4)
+triangulo_inv(5)
+triangulo_inv(6)
