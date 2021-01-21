4aed96f40dd872db3878786a75f1d6b9ea2d1c52
mariannaflores <68918978+mariannaflores@users.noreply.github.com>
2021-01-21 17:30:17 -0600

    Update Ejercicio 1.py

--- Lab1/Ejercicio 1.py
+++ Lab1/Ejercicio 1.py
@@ -1,12 +1,11 @@
 def triangulo_inv(n):
-
     for i in range(n,0,-1):
         for j in range(n-i):
             print(' ', end=' ') 
-
         for j in range(2*i-1):
             print('*',end=' ') 
-        print() 
+        print()
+ 
 
 triangulo_inv(4)
 triangulo_inv(5)
