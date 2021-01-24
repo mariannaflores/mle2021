b554d699497a43bf1f5b2cd417bccd99cce22589
mariannaflores <68918978+mariannaflores@users.noreply.github.com>
2021-01-24 16:26:40 -0600

    Update Ejercicio 4.py

--- Lab1/Ejercicio 4.py
+++ Lab1/Ejercicio 4.py
@@ -1,16 +1,15 @@
 from time import process_time 
 
 def primos(n):
-
-    is_prime = [False, False] + [True] * (n - 1)
+    primes = [False, False] + [True] * (n - 1)
     p = [2]
     for j in range(4, n + 1, 2):
-        is_prime[j] = False
+        primes[j] = False
     for i in range(3, n + 1, 2):
-        if is_prime[i]:
+        if primes[i]:
             p.append(i)
             for j in range(i * i, n + 1, i):
-                is_prime[j] = False
+                primes[j] = False
     print(p)
 
 t1_start = process_time()
